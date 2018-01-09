6ef81600af86a11bc216e7934e7c666e19dd104c
hue9010 <hue9010@gmail.com>
2018-01-09 16:55:13 +0900

    initial commit

---
+++ src/main/java/trello/JavaTrelloApplication.java
@@ -0,0 +1,12 @@
+package trello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTrelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTrelloApplication.class, args);
+	}
+}
